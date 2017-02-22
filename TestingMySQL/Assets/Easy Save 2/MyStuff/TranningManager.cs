3381cd214665a07ffbf90105feb2fc3e0faab7a0
n1troo <n1troov4@gmail.com>
2017-02-22 15:38:53 +0100

    dodawanie do panelu
    Dodanwanie do panelu prefabsów

---
+++ TestingMySQL/Assets/Easy Save 2/MyStuff/TranningManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TranningManager : MonoBehaviour
+{
+    private RectTransform[] panels;
+    public GameObject tranningPrefab; 
+
+	void Start ()
+    {
+        panels = GetComponentsInChildren<RectTransform>();
+
+        foreach (var ss in panels)
+        {
+            if (ss.name == "PanelMiddle")
+            {
+
+                for (int i = 0; i < 4; i++)
+                {
+                    GameObject pref = (GameObject)Instantiate(tranningPrefab, transform.position, transform.rotation) as GameObject;
+                    pref.transform.SetParent(ss.transform); 
+                }
+            }
+        }
+	}
+}
